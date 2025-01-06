2b1d2d2c8218ed611e09743d8dbc559f8532c2c2
Lev <lev.bereza@gmail.com>
2025-01-06 17:03:49 +0200

    Connecting to the database

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@
 
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
+const connectDb = require('./config/connectDb');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+connectDb();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -14,6 +18,9 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+mongoose.connection.once('open', () => {
+  console.log('Connected to the Database');
+  app.listen(PORT, async () => {
+    console.log(`Server started on port ${PORT}`);
+  });
 });
